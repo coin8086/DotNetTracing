fd2d07c88ca7e704b7e1b94577c765eca9b3913b
Robert Zhang <louirobert@gmail.com>
2018-11-29 13:18:31 +0800

    Implement tracing sample for console.

--- TracingConsole/Program.cs
+++ TracingConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace TracingConsole
 {
@@ -10,6 +11,19 @@ namespace TracingConsole
     {
         static void Main(string[] args)
         {
+            //Add listener by code
+            //Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            //Debug.AutoFlush = true;
+
+            Trace.WriteLine("I'm tracing!");
+            Console.WriteLine("I'm main!");
+            Debug.WriteLine("I'm debugging!");
+
+            TraceSource ts = new TraceSource("MyTraceSource");
+            ts.TraceEvent(TraceEventType.Information, 123, "My trace event.");
+            ts.TraceEvent(TraceEventType.Error, 123, "My trace error event.");
+            ts.Flush();
+            ts.Close();
         }
     }
 }
