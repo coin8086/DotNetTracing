76a7d004e372cb48575159a0577eecd74973b39e
Robert Zhang <louirobert@gmail.com>
2019-05-16 11:28:53 +0800

    Update code and config, adding more comments.

--- TracingConsole/Program.cs
+++ TracingConsole/Program.cs
@@ -12,21 +12,28 @@ namespace TracingConsole
     {
         static void Main(string[] args)
         {
-            //Add listener by code
+            //Add listener by code. See App.config for configuration in XML.
             //Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
             //Debug.AutoFlush = true;
 
+            //To add listeners for default tracing methods, config "listeners"
+            //element under "trace" element in App.config.
             Trace.WriteLine("I'm tracing!");
             Console.WriteLine("I'm main!");
             Debug.WriteLine("I'm debugging!");
 
-            //Trace with TraceSource
-            //TraceSource ts = new TraceSource("MyTraceSource");
-            //ts.TraceEvent(TraceEventType.Information, 123, "My trace event.");
-            //ts.TraceEvent(TraceEventType.Error, 123, "My trace error event.");
-            //ts.Flush();
-            //ts.Close();
+            //Trace with TraceSource in code. "MyTraceSource" is configed in
+            //App.config.
+            TraceSource ts = new TraceSource("MyTraceSource");
+            ts.TraceEvent(TraceEventType.Information, 123, "My trace event.");
+            ts.TraceEvent(TraceEventType.Error, 123, "My trace error event.");
+            ts.Flush();
+            ts.Close();
 
+            //MyTrace uses the same "MyTraceSource" as above code. Attention:
+            //there has to be <trace autoflush="true" ...> in App.config to
+            //see the output from MyTrace.TraceInfo/Error, though it uses the
+            //same "MyTraceSource" like the above code.
             MyTrace.TraceInfo("MyTrace works!");
             MyTrace.TraceError("MyTrace(error) works!");
         }
